6fb1af2ddde1ef10b511db53e9ec164fc0e543be
originecode <42325924+originecode@users.noreply.github.com>
2019-03-01 11:23:00 +0100

    Swipe improvments

--- docs/a_pre.js
+++ docs/a_pre.js
@@ -61,10 +61,9 @@ function swipedetect(el, callback){
     startY,
     distX,
     distY,
-    threshold = 30, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
+    threshold = 30,     // default : 150 => minimum distance
+    restraint = 70,     // default : 100 => error distance
+    allowedTime = 300,  // default : 300 => maximum time to move
     startTime,
     handleswipe = callback || function(swipedir){};
   
@@ -74,25 +73,24 @@ function swipedetect(el, callback){
         dist = 0;
         startX = touchobj.pageX;
         startY = touchobj.pageY;
-        startTime = new Date().getTime() ;// record time when finger first makes contact with surface
+        startTime = new Date().getTime();
         e.preventDefault();
     }, false);
   
     touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault(); // prevent scrolling when inside DIV
+        e.preventDefault();
     }, false);
-  
     touchsurface.addEventListener('touchend', function(e){
         var touchobj = e.changedTouches[0];
-        distX = touchobj.pageX - startX ;// get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY ;// get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime ;// get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right';// if dist traveled is negative, it indicates left swipe
+        distX = touchobj.pageX - startX ;
+        distY = touchobj.pageY - startY ;
+        elapsedTime = new Date().getTime() - startTime ;
+        if (elapsedTime <= allowedTime){
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+                swipedir = (distX < 0)? 'left' : 'right';
             }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down';// if dist traveled is negative, it indicates up swipe
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){
+                swipedir = (distY < 0)? 'up' : 'down';
             }
         }
         handleswipe(swipedir);

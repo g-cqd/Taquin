e7b018c635c792e5a01057019675608714273428
Mélanie M <marquesmelanie5@gmail.com>
2019-03-09 23:22:46 +0100

    Add files via upload

--- python/interfaceGraphique.py
+++ python/interfaceGraphique.py
@@ -2,16 +2,11 @@
 # -*-coding:utf-8 -*
 import sys
 from PyQt5 import QtGui, QtWidgets,QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel,QComboBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel,QComboBox,QCheckBox,QMessageBox
 from PyQt5.QtGui import QFontDatabase,QFont
 from PyQt5.QtCore import QSize,Qt,QPropertyAnimation
 from random import shuffle
 from math import sqrt,ceil
-import time
-
-
-from random import shuffle
-from math import ceil
 from collections import OrderedDict
 import time
 
@@ -20,7 +15,9 @@ class Taquin:
 		self.environment = environment
 		self.previous = previous
 		self.inv = None
+		self.dis = None
 		self.man = None
+		self.h = None
 		if previous == None:
 			self.path = "_"
 			self.g = 0
@@ -30,9 +27,8 @@ class Taquin:
 			self.g = previous.g + 1
 			self.sequence = previous.sequence.copy()
 			self.moveTile(move)
-			self.inv,self.man = self.details()
-		self.moves = self.findMoves(True)
-		self.h = self.man
+			self.inv,self.dis,self.man,self.h = self.details()
+		self.moves = self.findMoves()
 		self.f = self.h + self.g
 	def coordinates(self, content=0):
 		width = self.environment.sizes[0]
@@ -43,29 +39,38 @@ class Taquin:
 			y = ceil((index + 1) / width) - 1
 			x = index - (y * width)
 			return [x, y]
+
 	def details(self):
 		width, length = self.environment.sizes
 		weightings = self.environment.weightings
 		sequence = self.sequence
 		inv = 0
+		dis = 0
 		man = 0
+		h = 0
 		for weighting in weightings:
 			k = 0
-			stepMan = 0
-			for i, e in enumerate(sequence):
-				for j in range(i+1, length):
-					if (e!=0 and sequence[j] != 0 and e > sequence[j]):
-						inv += 1
-				if (i > 0):
+			stepH = 0
+			for i in range(0,length):
+				stepMan = 0
+				if weighting == weightings[0]:
+					for j in range(i+1,length):
+						if sequence[i] != 0 and sequence[j] != 0 and sequence[i] > sequence[j]: inv += 1
+					if sequence[i] != 0 and sequence[i] != (i+1):
+						dis += 1
+				if i > 0:
 					pos = self.coordinates(i)
-					x = i%width
+					x = i % width
 					coords = (((width - 1) if x == 0 else (x - 1)), ceil(i / width) - 1)
-					stepMan += (abs(pos[0]-coords[0]) + abs(pos[1]-coords[1])) * weighting[0][k]
+					stepMan += (abs(pos[0] - coords[0]) + abs(pos[1] - coords[1]))
+					if weighting == weightings[0]: man += stepMan
+					stepH += weighting[0][k] * stepMan
 					k += 1
-			if (weighting[1] > 1):
-				stepMan /= weighting[1]
-			man += stepMan
-		return (inv,man)
+			if weighting[1] > 1: stepH /= weighting[1]
+			if weighting[2] == 7: h += dis
+			else: h += stepH
+		return [inv,dis,man,h]
+
 	def findMoves(self,flex=False):
 		limit = self.environment.sizes[0] - 1
 		coords = self.coordinates()
@@ -88,7 +93,7 @@ class Taquin:
 		sequence[y] = 0
 	def valid(self):
 		width = self.environment.sizes[0]
-		self.inv,self.man = self.details()
+		self.inv,self.dis,self.man,self.h = self.details()
 		inv = self.inv
 		row = abs(self.coordinates()[1] - width)
 		return True if (((width % 2 == 1) and (inv % 2 == 0)) or ((width % 2 == 0) and ((row % 2 == 1) == (inv % 2 == 0)))) else False
@@ -121,8 +126,7 @@ class Taquin:
 		printable += ("|  moves . : {}\n").format(self.moves)
 		printable += ("|  g ..... : {}\n").format(self.g)
 		printable += ("|  h ..... : {}\n").format(self.h)
-		printable += ("|  f ..... : {}").format(self.f)
-		printable += "\n"
+		printable += ("|  f ..... : {}\n").format(self.f)
 		return printable
 
 
@@ -168,45 +172,41 @@ class Environment:
 						weight -= 1
 						j += width
 			if index == 6:
-					pi = [1] * length
+				pi = [1] * length
+			if index == 7:
+				pi = [0] * length
 			if (len(pi)>0):
 				weightings.append((pi,rho,index))
 		return weightings
 	def correct(self):
 		for move in self.moves:
-			move.inv,move.man = move.details()
-			move.h = move.man
+			move.inv,move.dis,move.man,move.h = move.details()
 			move.f = move.g + move.h
+	
+	
+	
 	def aStar(self):
+		print(self.moves[-1])
 		queue = OrderedDict()
 		queue[self.moves[-1].f] = [self.moves[-1]]
 		while (True):
-			# Retourne le premier élément dans liste des clés
 			k = list(queue.keys())[0]
-			# L'état à traiter est la premiere valeur contenu par la clé
-			shouldBeExpanded = queue[ k ][0]
-			# Suppression de cet état dans le dictionnaire
-			del queue[ k ][0]
-			# Si la clé est vide : la supprimer
-			if queue[ k ] == []: del queue[ k ]
-			# Découverte des enfants de l'état à expanser
+			shouldBeExpanded = queue[k][0]
+			del queue[k][0]
+			if queue[k] == []: del queue[k]
 			children = shouldBeExpanded.children()
-			# Si l'état retourne un enfant et non une liste d'enfants
-			# alors c'est la solution donc l'enregistrer et le retourner
 			if isinstance(children,Taquin):
 				print(children)
 				self.end.append(children)
 				return self.end[-1]
-			# Sinon ajouter les enfants dans le dictionnaire
 			else:
 				for child in children :
-					if(child.f in queue):
-						# Si la clé existe déjà, ajouter l'enfant aux éléments que la clé contient déjà
-						queue[child.f].append(child)
-					else:
-						# Sinon initialiser la clé avec une liste contenant l'enfant
-						queue[child.f] = [child]
+					if(child.f in queue): queue[child.f].append(child)
+					else: queue[child.f] = [child]
 				queue = OrderedDict( sorted( queue.items(), key=lambda t: t[0]))
+	
+
+	
 	def expand(self,function,decomposition=0):
 		if (decomposition==0):
 			print("\n\n")
@@ -229,6 +229,9 @@ class Environment:
 				print(("Duration : {}").format(time.time() - start))
 				print("\n\n.........................................\n")
 			return results
+	
+	
+	
 	def play(self,move):
 		self.moves.append(Taquin(self,self.moves[-1],move))
 		return self.moves[-1]
@@ -244,6 +247,19 @@ class Fenetre(QWidget):
 		self.t = []
 		self.mode = mode
 		self.label3Present = False
+		self.labelCoupsPresent = False
+		self.LabelManhattanPresent = False
+		self.LabelInvPresent = False
+		self.LabelDisPresent = False
+		self.h1state = False
+		self.h2state = False
+		self.h3state = False
+		self.h4state = False
+		self.h5state = False
+		self.h6state = False
+		self.heuristiques = []
+		self.boutonEnCouleur = None
+
 		#Chargement de la police inter bold
 		id = QFontDatabase.addApplicationFont("/Users/melaniemarques/Downloads/Inter-3/Inter (TTF)/Inter-Bold.ttf")
 		fontstr = QFontDatabase.applicationFontFamilies(id)[0]
@@ -256,7 +272,7 @@ class Fenetre(QWidget):
 
 
 		# Label
-		label = QLabel('Paramètres', self)
+		label = QLabel('Settings : ', self)
 		label.setFont(self.font)
 		label.move(50,50)
 
@@ -269,20 +285,25 @@ class Fenetre(QWidget):
 		mode.setFont(QFont(fontstr, 14))
 		mode.move(50,160)
 
+		heuristiques = QLabel('Heuristics : ',self)
+		heuristiques.setFont(QFont(fontstr,14))
+		heuristiques.move(50,210)
+
+
 		# ComboBox
 		ComboBox = QComboBox(self)
 		ComboBox.addItem("3 ")
 		ComboBox.addItem("4 ")
 		ComboBox.addItem("5 ")
 		ComboBox.addItem("6 ")
-		ComboBox.setStyleSheet('color: black;background-color: rgb(232,232,232);selection-background-color: rgb(255,255,255)')
+		ComboBox.setStyleSheet('color: black;background-color: rgb(232,232,232);selection-background-color: rgb(255,255,255);')
 		ComboBox.setFixedSize(QSize(85,20))
 		ComboBox.move(150,110)
 		ComboBox.activated[str].connect(self.selectionDimensions)
 		#Mode : 
 		ComboBoxM = QComboBox(self)
 		ComboBoxM.addItem("manual")
-		ComboBoxM.addItem("auto")
+		ComboBoxM.addItem("pilot")
 		ComboBoxM.setStyleSheet('color: black;background-color: rgb(232,232,232);selection-background-color: rgb(255,255,255)')
 		ComboBoxM.setFixedSize(QSize(85,20))
 		ComboBoxM.move(150,160)
@@ -295,7 +316,7 @@ class Fenetre(QWidget):
 		generate.move(700,320)
 		
 		generate.clicked.connect(self.appuiBoutonGenerate)
-		generate.clicked.connect(self.animation)
+		#generate.clicked.connect(self.animation)
 
 		#Clear console
 		clear = QPushButton('Solution',self)
@@ -304,6 +325,37 @@ class Fenetre(QWidget):
 		clear.setStyleSheet('color: rgb(179,179,179);background-color: rgb(232,232,232);border: none; border-radius: 4px;')
 		clear.move(805,320) 
 		clear.clicked.connect(self.Solution)
+
+		h1 = QCheckBox('H.1',self)
+		h1.setFont(QFont(fontstr,12))
+		h1.move(150,210)
+		h1.stateChanged.connect(self.selectionH1)
+
+		h2 = QCheckBox('H.2',self)
+		h2.setFont(QFont(fontstr,12))
+		h2.move(150,235)
+		h2.stateChanged.connect(self.selectionH2)
+
+		h3 = QCheckBox('H.3',self)
+		h3.setFont(QFont(fontstr,12))
+		h3.move(150,260)
+		h3.stateChanged.connect(self.selectionH3)
+
+		h4 = QCheckBox('H.4',self)
+		h4.setFont(QFont(fontstr,12))
+		h4.move(195,210)
+		h4.stateChanged.connect(self.selectionH4)
+
+		h5 = QCheckBox('H.5',self)
+		h5.setFont(QFont(fontstr,12))
+		h5.move(195,235)
+		h5.stateChanged.connect(self.selectionH5)
+
+		h6 = QCheckBox('H.6',self)
+		h6.setFont(QFont(fontstr,12))
+		h6.move(195,260)
+		h6.stateChanged.connect(self.selectionH6)
+
 		self.show()
 
 	def positionTuileRepereOrthonorme(self,numeroTuile, taquinL):
@@ -338,13 +390,13 @@ class Fenetre(QWidget):
 		
 		for item in cp:
 			if (item =="R"):
-				numeros.append([self.a.moves[-1].sequence[positionO-1],'r'])
+				numeros.append([self.a.moves[-1].sequence[positionO-1],'R'])
 			if (item =="L"):
-				numeros.append([self.a.moves[-1].sequence[positionO+1],'l'])
+				numeros.append([self.a.moves[-1].sequence[positionO+1],'L'])
 			if (item =="D"):
-				numeros.append([self.a.moves[-1].sequence[positionO-int(self.texte)],'d'])
+				numeros.append([self.a.moves[-1].sequence[positionO-int(self.texte)],'D'])
 			if (item =="U"):
-				numeros.append([self.a.moves[-1].sequence[positionO+int(self.texte)],'u'])
+				numeros.append([self.a.moves[-1].sequence[positionO+int(self.texte)],'U'])
 		return numeros
 	
 
@@ -356,46 +408,100 @@ class Fenetre(QWidget):
 				ok = False
 			i+=1
 		return ok
-		
-	def appuiBoutonGenerate(self):
-		#On supprime l'ancienne génération de taquin :
-		if(self.label3Present!=False):
-			self.label3.deleteLater()
-			self.label3Present = False
 
-		for k in self.box:
-			k.deleteLater()
-		self.box = []       
-		self.fin= False
-		DimTaquin = int(self.texte)
-		self.nbCoupsJoues = 0
-		self.a = Environment(DimTaquin,[1])
+	def operationColoration(self):
+		if(self.boutonEnCouleur!=None):
+			self.boutonEnCouleur.setStyleSheet('color: rgb(170,170,170);background-color: rgb(238,238,238);border: none; border-radius: 4px;')
+			self.boutonEnCouleur = None
+		self.a.expand(self.a.aStar,0)
+		caseAColorer = self.a.end[-1].path[self.nbCoupsJoues+1]
+		verif = self.ok()
 		i = 0
-		x = 0
-		y = 0
-		print(self.a.moves[-1].sequence)
-	   #On génère les boutons du Taquin : 
-		while(i<(DimTaquin*DimTaquin)):
-			if(i%DimTaquin==0):
-				x = 0
-				y+=42
-			if(self.a.moves[-1].sequence[i]!=0):
-				kk = QPushButton(str(self.a.moves[-1].sequence[i]),self)
-				kk.setFont(self.font)
-				kk.setFixedSize(QSize(40,40))
-				kk.setStyleSheet('color: rgb(170,170,170);background-color: rgb(238,238,238);border: none; border-radius: 4px;')
-				kk.move(700+x*42,1*y)
-				self.box.append(kk)
-			x+=1
+		while(i<len(verif)):
+			if((self.mode == 'pilot')and(caseAColorer==verif[i][1])):
+				numeroAColorer = verif[i][0]				
 			i+=1
-
 		for item in self.box:
-			item.clicked.connect(self.appuiBoutonsTaquin)
-			item.show()
+			print('item : ',item.text())
+			print('numero : ',numeroAColorer)
+			if(int(item.text())==numeroAColorer):						
+				self.boutonEnCouleur = item
+				self.boutonEnCouleur.setStyleSheet('color: rgb(255,255,255);background-color: rgb(135,206,250);border: none; border-radius: 4px;')
+
+		
+		
+
+	def appuiBoutonGenerate(self):
+		if(len(self.heuristiques)!=0):
+			self.nbCoupsJoues = 0
+		#On supprime l'ancienne génération de taquin :
+			if(self.label3Present!=False):
+				self.label3.deleteLater()
+				self.label3Present = False
+		#On regarde si le label de coups est présent, si oui on le delete
+			if(self.labelCoupsPresent!=False):
+				self.labelNbCoupsJoues.clear()			
+				self.labelCoupsPresent = False
+		#On regarde si le label Manhattan est présent, si oui on le delete
+			if(self.LabelManhattanPresent!=False):
+				self.LabelManhattan.clear()			
+				self.LabelManhattanPresent = False
+		#On regarde si le label Inv est présent, si oui on le delete
+			if(self.LabelInvPresent!=False):
+				self.LabelInv.clear()			
+				self.LabelInvPresent = False
+		
+			if(self.LabelDisPresent!=False):
+				self.LabelDis.clear()			
+				self.LabelDisPresent = False
+
+
+		
+		#On delete les boutons précédents du taquin : 		
+			for k in self.box:
+				k.deleteLater()
+		
+			self.box = []       
+			self.fin= False
+			DimTaquin = int(self.texte)
+		
+			self.a = Environment(DimTaquin,self.heuristiques)
+			i = 0
+			x = 0
+			y = 0
+	   #On génère les boutons du Taquin : 
+			while(i<(DimTaquin*DimTaquin)):
+				if(i%DimTaquin==0):
+					x = 0
+					y+=42
+				if(self.a.moves[-1].sequence[i]!=0):
+					kk = QPushButton(str(self.a.moves[-1].sequence[i]),self)
+					kk.setFont(self.font)
+					kk.setFixedSize(QSize(40,40))
+					kk.setStyleSheet('color: rgb(170,170,170);background-color: rgb(238,238,238);border: none; border-radius: 4px;')
+					kk.move(700+x*42,1*y)
+					self.box.append(kk)
+				x+=1
+				i+=1
+
+			for item in self.box:
+				item.clicked.connect(self.appuiBoutonsTaquin)
+				item.show()
+			if(self.mode =='pilot'):
+				self.operationColoration()
+		else :
+			print('on non')
+			msg = QMessageBox.warning(self, 'ERROR', "Please enter heuristic(s)", QMessageBox.Ok)
 			
+ 
+			#msg.setText("Test")
+			
+			#error_dialog = QtWidgets.QErrorMessage()
+			#error_dialog.showMessage('Oh no!')
+		
+	
 		
 		"""if(self.mode == 'auto'):
-
 			a.expand()
 			self.solution = a.end.path
 			label3 = QLabel("Path : %s"%(self.solution),self)
@@ -408,15 +514,13 @@ class Fenetre(QWidget):
 			
 		
 	def appuiBoutonsTaquin(self):
-
-		if((self.fin!=True) and (self.mode == 'manual')):
+		#print("on rentre")
+		if(self.fin!=True):
+			#Nombre de coups effectues : 			
 			ok = False
 			#On récupère le numéro du bouton appuyé : 
 			sender = self.sender()
-
-			#on vérifie qu'on peut bouger le bouton
-			
-			
+			#on vérifie qu'on peut bouger le bouton			
 			verif = self.ok() 
 			i = 0
 			move = "_"
@@ -426,7 +530,6 @@ class Fenetre(QWidget):
 					break
 				i+=1
 
-
 			#Si on peut bouger, on bouge le bouton : 
 			if(ok == True):
 				for item in self.box:
@@ -435,31 +538,75 @@ class Fenetre(QWidget):
 						x = BoutonClique.x()
 						y = BoutonClique.y()
 					 
-				if(verif[i][1]=='r'):
+				if(verif[i][1]=='R'):
 					BoutonClique.move(x+42,y)
 					move = "R"     
-				if(verif[i][1]=='l'):
+				if(verif[i][1]=='L'):
 					BoutonClique.move(x-42,y)
 					move = "L"
-				if(verif[i][1]=='u'):
+				if(verif[i][1]=='U'):
 					BoutonClique.move(x,y-42)
 					move = "U"
-				if(verif[i][1]=='d'):
+				if(verif[i][1]=='D'):
 					BoutonClique.move(x,y+42)
 					move ="D" 
 				self.a.play(move)
-				self.nbCoupsJoues+=1
+				self.nbCoupsJoues +=1
 				#On vérifie si on est dans l'état final :
 				if(self.isItTheEnd()==True):
 					self.fin = True
 					for boutons in self.box:
-						boutons.setStyleSheet('color: rgb(255,255,255);background-color: rgb(0,128,0);border: none; border-radius: 4px;')
+						boutons.setStyleSheet('color: rgb(255,255,255);background-color: rgb(60,179,113);border: none; border-radius: 4px;')
+				else:
+					if(self.mode == 'pilot'):
+						self.operationColoration()
+			#Label nombre de coups joués
+			self.labelNbCoupsJoues = QLabel("Coups:\n %d"%(self.a.moves[-1].g),self)
+			self.labelNbCoupsJoues.setFont(self.font)
+			self.labelNbCoupsJoues.setStyleSheet('background-color: rgb(238,238,238); border-radius: 4px;')
+			self.labelNbCoupsJoues.setAlignment(Qt.AlignCenter)
+			self.labelNbCoupsJoues.setFixedSize(QSize(110,50))
+			self.labelNbCoupsJoues.move(50,350)
+			self.labelNbCoupsJoues.show()
+			self.labelCoupsPresent = True
+
+			#Label Manhattan : 
+			self.LabelManhattan = QLabel("Manhattan:\n%d"%(self.a.moves[-1].man),self)
+			self.LabelManhattan.setFont(self.font)
+			self.LabelManhattan.setStyleSheet('background-color: rgb(238,238,238); border-radius: 4px;')
+			self.LabelManhattan.setAlignment(Qt.AlignCenter)
+			self.LabelManhattan.setFixedSize(QSize(110,50))
+			self.LabelManhattan.move(320,350)
+			self.LabelManhattan.show()
+			self.LabelManhattanPresent = True
+
+			#Label Inv : 
+			self.LabelInv = QLabel("Inversions:\n %d"%(self.a.moves[-1].inv),self)
+			self.LabelInv.setFont(self.font)
+			self.LabelInv.setStyleSheet('background-color: rgb(238,238,238); border-radius: 4px;')
+			self.LabelInv.setAlignment(Qt.AlignCenter)
+			self.LabelInv.setFixedSize(QSize(110,50))
+			self.LabelInv.move(455,350)
+			self.LabelInv.show()
+			self.LabelInvPresent = True
+
+			#Label Dis : 
+			self.LabelDis = QLabel("Desordre:\n %d"%(self.a.moves[-1].inv),self)
+			self.LabelDis.setFont(self.font)
+			self.LabelDis.setStyleSheet('background-color: rgb(238,238,238); border-radius: 4px;')
+			self.LabelDis.setAlignment(Qt.AlignCenter)
+			self.LabelDis.setFixedSize(QSize(110,50))
+			self.LabelDis.move(185,350)
+			self.LabelDis.show()
+			self.LabelDisPresent = True
+
+
 
 
 
 		
-	def animation(self):
-		if(self.mode =='auto'):
+	"""def animation(self):
+		if(self.mode =='pilot'):
 			self.a.expand()
 			self.solution = self.a.end.path
 			label3 = QLabel("Path : %s"%(self.solution),self)
@@ -528,7 +675,7 @@ class Fenetre(QWidget):
 				i+=1
 			
 			for boutons in self.box:
-				boutons.setStyleSheet('color: rgb(255,255,255);background-color: rgb(0,128,0);border: none; border-radius: 4px;')
+				boutons.setStyleSheet('color: rgb(255,255,255);background-color: rgb(0,128,0);border: none; border-radius: 4px;')"""
 
 	def Solution(self):
 		if(self.label3Present!=False):
@@ -539,8 +686,11 @@ class Fenetre(QWidget):
 		self.solution = self.a.end[-1].path[self.nbCoupsJoues+1:len(self.a.end[-1].path)]
 		self.label3 = QLabel("Solution : _ %s"%(self.solution),self)
 		self.label3.setFont(self.font)
-		self.label3.move(50,400)
+		self.label3.setStyleSheet('background-color: rgb(238,238,238); border-radius: 4px;')
+		self.label3.setFixedSize(565,50)
+		self.label3.move(50,425)
 		self.label3.show()
+
 		self.label3Present = True
 		#print(a.end.path)
 		print(self.nbCoupsJoues)
@@ -552,7 +702,43 @@ class Fenetre(QWidget):
 		self.texte = texte
 	def selectionMode(self,mode):       
 		self.mode = mode
-		
+
+	def selectionH1(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(1)
+		elif state != Qt.Checked:
+			self.heuristiques.remove(1)
+
+	def selectionH2(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(2)			
+		elif state != Qt.Checked:
+			self.heuristiques.remove(2)
+
+	def selectionH3(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(3)		
+		elif state != Qt.Checked:
+			self.heuristiques.remove(3)
+
+	def selectionH4(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(4)				
+		elif state != Qt.Checked:
+			self.heuristiques.remove(4)
+
+	def selectionH5(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(5)			
+		elif state != Qt.Checked:
+			self.heuristiques.remove(5)
+
+	def selectionH6(self,state):
+		if state == Qt.Checked:
+			self.heuristiques.append(6)			
+		elif state != Qt.Checked:
+			self.heuristiques.remove(6)
+	
 
 class __main__:
 	app = QApplication.instance() 
